311021a2931c780f2bfebb72800bf7d0c6bac557
magikcypress <cyp@rouquin.me>
2025-07-26 15:27:17 +0200

    Enhance CORS configuration in server/index.js by adding 'X-Client-Version' to allowed headers, improving support for client versioning in requests.

--- server/index.js
+++ server/index.js
@@ -130,7 +130,7 @@ const limiter = rateLimit({
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, Cache-Control');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, Cache-Control, X-Client-Version');
 
     if (req.method === 'OPTIONS') {
         return res.status(200).end();
